ffbf1f3d902a85a5c9f9a125caba98af31dd4949
takuizum <sep10.taku.izum@gmail.com>
2020-05-01 19:19:53 +0900

    add versioninfo and RCall check

--- src/init.jl
+++ src/init.jl
@@ -1,3 +1,10 @@
 i = open(".gitignore", "w")
-println(i, "*.code-workspace")
+println(i, "*.code-workspace\n.vscode/\n.DS_Store")
 close(i)
+
+# Version check
+versioninfo()
+
+# RCall check
+using RCall
+R"sessionInfo()"
